4631a9e234e9160b2d12665a2d0add59ae888935
JAKThaDev <jakthadev@Jds-MacBook-Pro.local>
2021-04-09 23:43:56 -0400

    Things are working

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server  = http.createServer((req, res) => {
     // so the responds object is already created when the object is recieved, This is why we dont  
     // Need to create it. The request can come from any client side application.  
     console.log(req.headers); 
-    res.setHeader = 200; 
+    res.statusCode = 200; 
     res.setHeader('Content-Type', 'text/html'); 
     res.end('<html><body><h1>Hello World!</h1></body></html>');
 }); 
